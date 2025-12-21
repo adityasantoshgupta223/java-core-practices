196ec8b99d07c48d9c42d67104c2c5fca0139c06
Aditya Gupta <adityasantoshgupta223@gmail.com>
2025-12-21 21:32:09 +0530

    Updated HashMap Syntax

--- collections/HashMapDemo.java
+++ collections/HashMapDemo.java
@@ -3,12 +3,13 @@ import java.util.HashMap;
 public class HashMapDemo {
    public static void main(String[] args) {
     
-    HashMap<String, Double> poplton = new HashMap();     // Map of Population of country in Billions 
+    HashMap<String, Double> poplton = new HashMap<>();     // Map of Population of country in Billions 
 
     // Guessed Values 
     poplton.put("China", 1.23);
     poplton.put("India", 1.4);
     poplton.put("USA", 0.34);
+    poplton.put("Japan", 0.34);
 
     System.out.println(poplton);
    }
