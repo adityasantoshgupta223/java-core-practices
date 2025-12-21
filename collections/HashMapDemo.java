65f942b0a9e7d43d050ef097ed6577ce837cfb1f
Aditya Gupta <adityasantoshgupta223@gmail.com>
2025-12-21 21:57:00 +0530

    HashMap Basics

--- collections/HashMapDemo.java
+++ collections/HashMapDemo.java
@@ -3,14 +3,35 @@ import java.util.HashMap;
 public class HashMapDemo {
    public static void main(String[] args) {
     
-    HashMap<String, Double> poplton = new HashMap<>();     // Map of Population of country in Billions 
+    HashMap<String, Double> population = new HashMap<>();     // Map of Population of country in Billions 
 
-    // Guessed Values 
-    poplton.put("China", 1.23);
-    poplton.put("India", 1.4);
-    poplton.put("USA", 0.34);
-    poplton.put("Japan", 0.34);
+    // Insert  Key-Value Pair  
+    population.put("China", 1.23);
+    population.put("India", 1.4);
+    population.put("USA", 0.34);
+    population.put("Japan", 0.34);
+    population.put("France", 0.12);
 
-    System.out.println(poplton);
+    System.out.println(population);
+
+    // Get Value of Key
+    System.out.println("China's Population : " + population.get("China"));
+
+    // Checking whether the specified Key exist in map or Not
+    System.out.println("China Exist in HashMap : " + population.containsKey("China"));
+
+    // Deletion by Key
+    population.remove("China");
+    population.remove("France");
+
+    System.out.println("HashMap After deletion : " + population);
+
+    // Updation
+    population.put("USA", 0.3);
+    System.out.println(population);
+
+    population.clear();
+
+    System.out.println(population);
    }
 }
