69b9d978546b8e2f02b8afd552cb7604d62caded
Aditya Gupta <adityasantoshgupta223@gmail.com>
2025-12-25 16:42:59 +0530

    Rename to ThreadVsRunnableDemo.java

---
+++ src/main/java/com/aditya/javacore/multithreading/ThreadVsRunnableDemo.java
@@ -0,0 +1,8 @@
+package com.aditya.javacore.multithreading;
+
+class ThreadVsRunnableDemo {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+}
