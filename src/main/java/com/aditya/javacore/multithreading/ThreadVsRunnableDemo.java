b6f900e5a56c4e830438d739bd55a2515499ec84
Aditya Gupta <adityasantoshgupta223@gmail.com>
2025-12-25 16:55:48 +0530

    Thread and Runnable

--- src/main/java/com/aditya/javacore/multithreading/ThreadVsRunnableDemo.java
+++ src/main/java/com/aditya/javacore/multithreading/ThreadVsRunnableDemo.java
@@ -1,8 +1,52 @@
 package com.aditya.javacore.multithreading;
 
-class ThreadVsRunnableDemo {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello World");
+class ThreadTask extends Thread{
+ private String msg;
+
+  ThreadTask(String msg){
+    this.msg = msg;
+  }
+   public void run(){
+    for(int i=0;i<100;i++){
+      System.out.println(msg + " From " + Thread.currentThread().getName());
     }
+   }
+
+}
+class RunnableTask implements Runnable{
+  private String msg;
+
+   public RunnableTask(String msg) {
+    this.msg = msg;
+   }
+  
+   public void print(){
+    System.out.println(msg + " From Runnable by " + Thread.currentThread().getName());
+   }
+
+   public void run(){
+         for(int i=0;i<100;i++){
+          print();
+         }
+   }
+}
+public class ThreadVsRunnableDemo {
+   public static void main(String[] args) {
+    
+  ThreadTask t1 = new ThreadTask("Hi");
+  ThreadTask t2 = new ThreadTask("Hello");
+
+
+  t1.start();
+  t2.start();
+
+  RunnableTask runTsk = new RunnableTask("Hi ");
+
+  Thread t3 = new Thread(runTsk);
+  Thread t4 = new Thread(runTsk);
+
+  t3.start();
+  t4.start();
+
+   }
 }
